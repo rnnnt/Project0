3849686820f4a6c9934374f068ebce55028a95ed
rnnnt <renato.ortiz@usm.cl>
2024-05-20 22:04:03 -0400

    admin

--- INFJMC/core/models.py
+++ INFJMC/core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Carrera(models.Model):
+    codigo_carrera = models.CharField(max_length=10, verbose_name='codigo')
+    nombre = models.CharField(max_length=80, verbose_name='nombre')
+    duracion = models.IntegerField(verbose_name='duracion', default=1) #min=0
+
+    def __str__(self) -> str:
+        return self.nombre
